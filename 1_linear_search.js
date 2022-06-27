0e6e24146968a18f82c66d82f104f96c34d85aad
Liam <backgroundpositioncenter@gmail.com>
2022-06-27 17:35:32 +0300

    Линейный поиск

--- 1_linear_search.js
+++ 1_linear_search.js
@@ -1,1 +1,15 @@
+const array = [1, 3, 5, 2, 7, 4, 9, 6, 8, 10];
+let counter = 0;
 
+function linearSearch(array, item) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      counter += 1;
+      return i;
+    }
+  }
+  return null;
+}
+
+console.log("Номер индекса:", linearSearch(array, 5));
+console.log("Кол-во итераций:", counter);
